1c2da86bc7f2d169055d74d07f6d22b4798fc587
akhil-singh28 <as1575828@gmail.com>
2026-04-03 19:11:02 +0530

    Initial Commit

---
+++ backend/app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pymysql
+
+app = FastAPI()
+
+# DB Connection
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="Akhil|2805",
+    database="emergency_db"
+)
+
+class Alert(BaseModel):
+    user_id: int
+    location: str
+
+@app.get("/")
+def home():
+    return {"message": "Backend Running Successfully 🚀"}
+
+@app.post("/send-alert")
+def send_alert(alert: Alert):
+    print("🚨 ALERT RECEIVED 🚨")
+
+    cursor = connection.cursor()
+
+    query = "INSERT INTO emergency_alerts (user_id, location) VALUES (%s, %s)"
+    cursor.execute(query, (alert.user_id, alert.location))
+
+    connection.commit()
+
+    return {"message": "Alert saved in database"}
