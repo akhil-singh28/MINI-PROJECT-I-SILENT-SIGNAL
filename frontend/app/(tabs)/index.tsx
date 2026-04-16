d38e2801d27772dacdf467741327f7b9c7ef540d
AKHIL PRATAP SINGH <as1575828@gmail.com>
2026-04-16 22:45:06 +0530

    Initial commit

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,132 +1,3 @@
-// import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
-// import { useState } from 'react';
-
-// export default function App() {
-
-//   const [sent, setSent] = useState(false);
-//   const handlePress = async () => {
-//     try {
-//       const response = await fetch("http://192.168.137.1:8000/send-alert", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({
-//           user_id: 101,
-//           location: "Mathura"
-//         })
-//       });
-
-//       const data = await response.json();
-//       console.log(data);
-
-//       setSent(true);
-//     } catch (error) {
-//       alert("Error connecting to server");
-//     }
-//   };
-
-//   const handleReset = () => {
-//     setSent(false);
-//   };
-
-//   return (
-//     <ScrollView contentContainerStyle={styles.container}>
-//       <Text style={styles.title}>🚨 Emergency</Text>
-//       <Text style={styles.subtitle}>
-//         {sent ? "Alert Sent Successfully" : "Tap the button to send an immediate alert"}
-//       </Text>
-
-//       <TouchableOpacity
-//         style={[styles.button, sent && styles.greenButton]}
-//         onPress={handlePress}
-//         disabled={sent}
-//       >
-//         <Text style={styles.buttonText}>
-//           {sent ? "✔ SENT" : "⚡ HELP NOW"}
-//         </Text>
-//       </TouchableOpacity>
-
-//       {sent && (
-//         <TouchableOpacity style={styles.resetButton} onPress={handleReset}>
-//           <Text style={styles.resetText}>RESET</Text>
-//         </TouchableOpacity>
-//       )}
-
-//     </ScrollView>
-//   );
-// }
-// const styles = StyleSheet.create({
-
-//   container: {
-//     flexGrow: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     paddingTop: 60,
-//     paddingBottom: 40,
-//     backgroundColor: '#0f172a'
-//   },
-
-//   title: {
-//     fontSize: 28,
-//     color: '#ffffff',
-//     fontWeight: 'bold',
-//     marginBottom: 15
-//   },
-
-//   subtitle: {
-//     fontSize: 15,
-//     color: '#94a3b8',
-//     marginBottom: 40,
-//     textAlign: 'center',
-//     paddingHorizontal: 20
-//   },
-
-//   button: {
-//     width: 250,
-//     height: 250,
-//     borderRadius: 125,
-//     backgroundColor: '#ef2424',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-
-//     shadowColor: '#ef4444',
-//     shadowOffset: { width: 0, height: 0 },
-//     shadowOpacity: 0.8,
-//     shadowRadius: 20,
-//     elevation: 20
-//   },
-
-//   greenButton: {
-//     backgroundColor: '#22c55e',
-//     shadowColor: '#22c55e'
-//   },
-
-//   buttonText: {
-//     color: '#fff',
-//     fontSize: 25,
-//     fontWeight: 'bold',
-//     textAlign: 'center'
-//   },
-
-//   resetButton: {
-//     marginTop: 30,
-//     paddingVertical: 10,
-//     paddingHorizontal: 25,
-//     backgroundColor: '#8693a7',
-//     borderRadius: 10
-//   },
-
-//   resetText: {
-//     color: '#fff',
-//     fontSize: 20,
-//     fontWeight: 'bold'
-//   }
-// });
-
-
-
-
 import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Linking } from 'react-native';
 import { useState } from 'react';
 
@@ -136,7 +7,7 @@ export default function App() {
 
   //SMS FUNCTION
   const sendSMS = () => {
-    const phoneNumber = '6396775752'; // 👉 apna emergency wala number likh sakte h
+    const phoneNumber = '___________________________________'; // apna emergency wala number likh sakte h
     const message = 'Emergency! I need help. Location: Mathura';
 
     Linking.openURL(`sms:${phoneNumber}?body=${message}`);
@@ -144,8 +15,7 @@ export default function App() {
 
   const handlePress = async () => {
     try {
-      const response = await fetch("http://192.168.137.1:8000/send-alert", {   // 👉 apna backend URL likhna h sbko   AB- 10 WLA YE HAI- "http://172.16.166.5:8000/send-alert"       (HOSTEL WLA YE HAI-  "192.168.137.1/send-alert")
-        method: "POST",
+      const response = await fetch("_______________________________________________", {   
         headers: {
           "Content-Type": "application/json"
         },
